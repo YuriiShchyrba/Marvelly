50603a9687da6a6a7d2dc6ab03093a6a56d28609
YuriiShchyrba <yurashchyrba@gmail.com>
2022-02-15 22:04:32 -0500

    Update socialModel.js

--- server/model/socialModel.js
+++ server/model/socialModel.js
@@ -1,6 +1,6 @@
 const {Pool} = require('pg');
 
-const PG_URI = 'postgres://nvjjxdsi:uzQarEYDxnA7-le-PqC6qX-5dPGpCLbz@castor.db.elephantsql.com/nvjjxdsi';
+
 
 const pool = new Pool({
     connectionString: PG_URI
